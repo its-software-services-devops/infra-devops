4f4e56b9d76860c7fd21a320b30d26869a093e8f
Seubpong Monsar <pjame.fb@gmail.com>
2021-05-14 21:27:49 +0700

    Added firewall rules

--- gce-rke-manager/terraforms/gcp-firewall-rules.tf
+++ gce-rke-manager/terraforms/gcp-firewall-rules.tf
@@ -1,6 +1,7 @@
-resource "google_compute_firewall" "k8s-nodes-allow-only-manager" {
-  name    = "k8s-nodes-allow-only-manager"
+resource "google_compute_firewall" "k8s-manager-to-cluster" {
+  name    = "k8s-manager-to-cluster"
   network = "default"
+  priority = 1000
 
   allow {
     protocol = "icmp"
@@ -8,17 +9,35 @@ resource "google_compute_firewall" "k8s-nodes-allow-only-manager" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "443", "6443", "10250", "10255"]
+    ports    = ["22", "443", "6443"]
   }
 
   source_tags = ["k8s-manager"]
   target_tags = ["k8s-master", "k8s-worker"]
 }
 
+resource "google_compute_firewall" "k8s-nodes-internode-connect" {
+  name    = "k8s-nodes-internode-connect"
+  network = "default"
+  priority = 1001
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443", "6443", "10250", "10255"]
+  }
+
+  source_tags = ["k8s-master", "k8s-worker"]
+  target_tags = ["k8s-worker", "k8s-master"]
+}
+
 resource "google_compute_firewall" "k8s-nodes-deny-public" {
   name    = "k8s-nodes-deny-public"
   network = "default"
-  priority = 1001
+  priority = 1002
 
   deny {
     protocol = "icmp"
