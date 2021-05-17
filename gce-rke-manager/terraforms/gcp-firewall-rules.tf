7aaffbd9b9d0be538dac0f26e2640f0d4e006214
Seubpong Monsar <pjame.fb@gmail.com>
2021-05-17 08:11:07 +0700

    Removed unused fw rules

--- gce-rke-manager/terraforms/gcp-firewall-rules.tf
+++ gce-rke-manager/terraforms/gcp-firewall-rules.tf
@@ -34,20 +34,6 @@ resource "google_compute_firewall" "k8s-nodes-internode-connect" {
   target_tags = ["k8s-worker", "k8s-master"]
 }
 
-resource "google_compute_firewall" "k8s-nodes-lb-connect" {
-  name    = "k8s-nodes-lb-connect"
-  network = "default"
-  priority = 1002
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443", "6443", "80"]
-  }
-
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  target_tags = ["k8s-worker"]
-}
-
 resource "google_compute_firewall" "k8s-nodes-deny-public" {
   name    = "k8s-nodes-deny-public"
   network = "default"
