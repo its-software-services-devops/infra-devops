020d9903fee8489867a0c493e62cf96574bb5e35
Seubpong Monsar <pjame.fb@gmail.com>
2021-05-28 19:42:31 +0700

    Secret cloud dns secret

--- gce-rke-cluster/deploy/deploy-certificates.bash
+++ gce-rke-cluster/deploy/deploy-certificates.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+kubectl create secret generic gcp-cloud-dns-account-key \
+--from-file=service-account.json=cloud-dns-rke-demo.json \
+-n cert-manager
+
 kubectl apply -f cluster-certs.yaml
