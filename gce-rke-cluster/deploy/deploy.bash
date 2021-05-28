854b695efc6dd2bbdbf00ae2af7b4135bcdb20f3
Seubpong Monsar <pjame.fb@gmail.com>
2021-05-28 14:15:11 +0700

    Added deployment script

---
+++ gce-rke-cluster/deploy/deploy.bash
@@ -0,0 +1,12 @@
+#/bin/bash
+
+CFG=kubeconfig
+terraform output its-rancher-demo-kube_config_yaml > ${CFG}.out
+
+LINECOUNT=$(cat ${CFG}.out | wc -l)
+let "TOLINE=${LINECOUNT}-1"
+sed -ne "2,${TOLINE}p" ${CFG}.out > ${CFG}
+
+export KUBECONFIG=$(pwd)/${CFG}
+
+./deploy-certificates.bash
