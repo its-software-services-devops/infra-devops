9123664f8d9cb35281bfdd6624f8e7aea96f1f6e
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-25 12:21:33 +0700

    Added Loki for Syslog

--- gce-rke-cluster/deploy/loki-syslog/deploy-loki-syslog.bash
+++ gce-rke-cluster/deploy/loki-syslog/deploy-loki-syslog.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 NS=loki-syslog
+SECRET=loki-gcs-bucket-key
 
 echo "####"
 echo "#### Deploying Loki for Syslog to [${NS}] ####"
+
+kubectl delete secret ${SECRET} -n ${NS}
+
+kubectl create secret generic ${SECRET} \
+--from-file=service-account.json=cloud-dns-rke-demo.json \
+-n ${NS}
 
 kubectl create ns ${NS}
 kubectl apply -f rendered-loki-syslog.yaml -n ${NS}
