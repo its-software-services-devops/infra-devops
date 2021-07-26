ca7b14a1e5769144671468b18f2e91ed2c5185fd
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-26 18:39:33 +0700

    Added kafka operator setup

--- gce-rke-cluster/setup/kafka-operator/setup-kafka-operator.bash
+++ gce-rke-cluster/setup/kafka-operator/setup-kafka-operator.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 NS=kafka-operator
+VERSION=0.24.0
 
 echo "####"
 echo "#### Setting Kafka Operator into [${NS}] ####"
 
 kubectl create ns ${NS}
-kubectl apply -f "https://strimzi.io/install/latest?namespace=${NS}" -n ${NS}
+
+kubectl apply -n ${NS} \
+-f https://github.com/strimzi/strimzi-kafka-operator/releases/download/${VERSION}/strimzi-crds-${VERSION}.yaml
+
+kubectl apply -f rendered-kafka-operator.yaml -n ${NS}
