a4b74715b1acd30d894411a7d016a3ed36c73e4e
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-27 09:38:50 +0700

    Added external-dns

--- gce-rke-manager/terraforms/gcp-clound-dns-zone.tf
+++ gce-rke-manager/terraforms/gcp-clound-dns-zone.tf
@@ -1,4 +1,8 @@
 resource "google_dns_managed_zone" "gcp-rke-demo" {
   name     = "gcp-rke-demo"
   dns_name = "gcp-rke-demo.its-software-services.com."
+
+  dnssec_config {
+      state = "on"
+  }
 }
