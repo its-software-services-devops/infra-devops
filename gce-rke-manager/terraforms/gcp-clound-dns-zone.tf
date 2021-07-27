e80520908e1e302c8c012a9ed8748f3ae2cdabda
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-27 09:54:15 +0700

    Added DNS zone

--- gce-rke-manager/terraforms/gcp-clound-dns-zone.tf
+++ gce-rke-manager/terraforms/gcp-clound-dns-zone.tf
@@ -8,6 +8,16 @@ resource "google_dns_managed_zone" "gcp-rke-demo" {
 }
 
 resource "google_dns_record_set" "gcp-rke-demo-ns" {
+  name = "axyshai-terraform-add.gcp-rke-demo.its-software-services.com."
+  type = "A"
+  ttl  = 300
+
+  managed_zone = google_dns_managed_zone.gcp-rke-demo.zone.name
+
+  rrdatas = "192.168.1.66"
+}
+
+resource "google_dns_record_set" "gcp-rke-demo-ns" {
   name = "gcp-rke-demo.its-software-services.com."
   type = "NS"
   ttl  = 300
