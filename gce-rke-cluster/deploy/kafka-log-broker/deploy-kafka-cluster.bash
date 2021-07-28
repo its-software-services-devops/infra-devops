95a8575f66a311d3558357986656a5886023d387
Seubpong Monsar <pjame.fb@gmail.com>
2021-07-28 17:14:37 +0700

    Added kafka topic

--- gce-rke-cluster/deploy/kafka-log-broker/deploy-kafka-cluster.bash
+++ gce-rke-cluster/deploy/kafka-log-broker/deploy-kafka-cluster.bash
@@ -10,3 +10,4 @@ kubectl create ns ${NS}
 kubectl apply -f kafka-cluster.yaml -n ${NS}
 kubectl apply -f kafka-syslog-topic.yaml -n ${NS}
 kubectl apply -f kafka-user.yaml -n ${NS}
+kubectl apply -f service.yaml -n ${NS}
