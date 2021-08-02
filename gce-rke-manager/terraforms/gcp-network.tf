eaa274404cfe81b8ac89b3888531dff94fc7055f
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-02 23:01:08 +0700

    Test terraform vpc

--- gce-rke-manager/terraforms/gcp-network.tf
+++ gce-rke-manager/terraforms/gcp-network.tf
@@ -1,5 +1,6 @@
-
+/*
 resource "google_compute_network" "team-b-vpc-network" {
   name = "team-b-vpc-network"
-  delete_default_routes_on_create = true
+  delete_default_routes_on_create = false
 }
+*/
