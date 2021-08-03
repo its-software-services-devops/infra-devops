a5c9bea0b2a60aa498b3edeb3e4f9e2af2aa7cd6
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-03 19:41:55 +0700

    Added FW rule allow ssh to manager

--- gce-rke-manager/terraforms/gcp-firewall-rules.tf
+++ gce-rke-manager/terraforms/gcp-firewall-rules.tf
@@ -34,6 +34,23 @@ resource "google_compute_firewall" "k8s-nodes-internode-connect" {
   target_tags = ["k8s-worker", "k8s-master"]
 }
 
+resource "google_compute_firewall" "k8s-manager-allow-ssh" {
+  name    = "k8s-manager-allow-ssh"
+  network = "team-a-vpc-network"
+  priority = 1002
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["k8s-manager"]
+}
+
 resource "google_compute_firewall" "k8s-nodes-deny-public" {
   name    = "k8s-nodes-deny-public"
   network = "team-a-vpc-network"
