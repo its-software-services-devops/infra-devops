6e71d7078ccf3b6b19e57548a869049860c612f4
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-05 22:00:32 +0700

    Added SA

--- gce-rke-manager/terraforms/gcp-service-account.tf
+++ gce-rke-manager/terraforms/gcp-service-account.tf
@@ -4,17 +4,11 @@ resource "google_service_account" "gcp-rke-demo-sa" {
   display_name = "gcp-rke-demo SA"
 }
 
-resource "google_project_iam_policy" "project" {
-  project     = "its-artifact-commons"
-  policy_data = data.google_iam_policy.node-gcp-rke-demo.policy_data
-}
+resource "google_project_iam_binding" "node-gcp-rke-demo" {
+  project = "its-artifact-commons"
+  role    = "roles/owner"
 
-data "google_iam_policy" "node-gcp-rke-demo" {
-  binding {
-    role = "roles/owner"
-
-    members = [
-      "serviceAccount:${google_service_account.gcp-rke-demo-sa.email}",
-    ]
-  }
+  members = [
+    "serviceAccount:${google_service_account.gcp-rke-demo-sa.email}",
+  ]
 }
