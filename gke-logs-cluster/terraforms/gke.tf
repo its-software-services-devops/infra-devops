26ab192f37c689b1e9c3f9702ae0271f13f39ca2
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-10 21:52:49 +0700

    Added new gke cluster

---
+++ gke-logs-cluster/terraforms/gke.tf
@@ -0,0 +1,33 @@
+resource "google_service_account" "etda-logs-monitoring-sa" {
+  account_id   = "etda-logs-monitoring-sa"
+  display_name = "Service Account for ETDA"
+}
+
+resource "google_container_cluster" "etda-logs-monitoring" {
+  name     = "etda-logs-monitoring"
+  location = "asia-southeast1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 3
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "etda-logs-basic-pool"
+  location   = "asia-southeast1"
+  cluster    = google_container_cluster.etda-logs-monitoring.name
+  node_count = 3
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.etda-logs-monitoring-sa.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
