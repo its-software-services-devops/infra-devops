881bb9e32c7a6a6045632a03b3bfc5fee1a39be0
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-13 21:18:46 +0700

    Refactored code

--- gke-logs-cluster/modules/node-pool/nodepool.tf
+++ gke-logs-cluster/modules/node-pool/nodepool.tf
@@ -1,35 +1,3 @@
-# IMPORTANT
-## Please BE CAREFUL when changing `keepers`. If you add a keeper
-## then this will cause a CREATION CASCADE! Since a new keeper
-## triggers a new `random_id` resource to generate then it will change
-## the name of the node pool, which will recreate that resource!
-## This is the preferred method for random naming now that
-## name-prefix is being deprecated!
-## IF YOU ADD A KEEPER YOU WILL NEED TO WARN DOWNSTREAM USERS THAT
-## IT WILL TRIGGER A CASCADING NODEPOOL REPLACEMENT.
-## The replacement should be safe because of the create_before_destroy
-## but will cause some questions and confusion.
-## All the current keepers are bound to params that will already cause
-## a -/+ operation for the nodepool.
-# https://github.com/terraform-providers/terraform-provider-google/issues/1054
-
-resource "random_id" "entropy" {
-  keepers = {
-    machine_type            = var.machine_type
-    name                    = var.name
-    region                  = var.region
-    disk_size               = var.disk_size_in_gb
-    tags                    = join(",", sort(var.node_tags))
-    disk_type               = var.disk_type
-    labels                  = jsonencode(var.node_labels)
-    initial_node_count      = var.initial_node_count
-    additional_oauth_scopes = join(",", sort(var.additional_oauth_scopes))
-    enable_secure_boot      = var.enable_secure_boot
-  }
-
-  byte_length = 2
-}
-
 locals {
   base_oauth_scope = [
     "https://www.googleapis.com/auth/devstorage.read_only",
@@ -44,7 +12,7 @@ locals {
 }
 
 resource "google_container_node_pool" "node_pool" {
-  name               = var.name #"${var.name}-${random_id.entropy.hex}"
+  name               = var.name
   cluster            = var.gke_cluster_name
   location           = var.region
   version            = var.kubernetes_version
