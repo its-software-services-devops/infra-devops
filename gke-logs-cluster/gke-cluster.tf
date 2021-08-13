9f7ae4d680d269c5620810d3a8fdb3e74a8c06c0
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-13 16:13:32 +0700

    Refactored code

--- gke-logs-cluster/gke-cluster.tf
+++ gke-logs-cluster/gke-cluster.tf
@@ -54,8 +54,8 @@ module "etda-logs-basic-pool" {
   region           = module.etda-logs-cluster.region
   gke_cluster_name = module.etda-logs-cluster.name
   machine_type     = "n1-standard-2"
-  min_node_count   = "1"
-  max_node_count   = "1"
+  min_node_count   = "2"
+  max_node_count   = "2"
   service_account_email = google_service_account.etda-logs-monitoring-sa.email
 
   # Match the Kubernetes version from the GKE cluster!
