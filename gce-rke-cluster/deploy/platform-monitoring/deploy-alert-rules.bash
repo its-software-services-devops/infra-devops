17ed8118b84d056eeb1d4ebbf266d27e7789276a
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-22 13:44:33 +0700

    Added certmanager alert rules

--- gce-rke-cluster/deploy/platform-monitoring/deploy-alert-rules.bash
+++ gce-rke-cluster/deploy/platform-monitoring/deploy-alert-rules.bash
@@ -6,6 +6,17 @@ CERT_MANAGER_MIXIN=cert-manager-mixin
 rm -rf ${CERT_MANAGER_MIXIN}
 git clone https://gitlab.com/uneeq-oss/${CERT_MANAGER_MIXIN}.git
 
+cat << 'EOF' > ${CERT_MANAGER_MIXIN}/config.libsonnet
+{
+  _config+:: {
+    certManagerCertExpiryDays: '21',
+    certManagerJobLabel: 'cluster-cert-manager',
+    certManagerRunbookURLPattern: 'https://gitlab.com/uneeq-oss/cert-manager-mixin/-/blob/master/RUNBOOK.md#%s',
+    grafanaExternalUrl: 'https://grafana-platform.gcp-rke-demo.its-software-services.com',
+  },
+}
+EOF
+
 docker run -v $(pwd)/${CERT_MANAGER_MIXIN}/:/data/ \
 bitnami/jsonnet:latest \
 -S /data/lib/alerts.jsonnet > ${CERT_MANAGER_MIXIN}.yaml
