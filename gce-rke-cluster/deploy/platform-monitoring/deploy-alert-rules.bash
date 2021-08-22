d03253fb7bde88bff21c01e1eb741e62c1ed52da
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-22 13:01:51 +0700

    Added certmanager alert rules

--- gce-rke-cluster/deploy/platform-monitoring/deploy-alert-rules.bash
+++ gce-rke-cluster/deploy/platform-monitoring/deploy-alert-rules.bash
@@ -4,8 +4,7 @@ NS=platform-monitoring
 CERT_MANAGER_MIXIN=cert-manager-mixin
 
 git clone https://gitlab.com/uneeq-oss/${CERT_MANAGER_MIXIN}.git
-#jsonnet -S ${CERT_MANAGER_MIXIN}/lib/alerts.jsonnet > ${CERT_MANAGER_MIXIN}.yaml
 
-docker run --name jsonnet bitnami/jsonnet:latest \
--v $(pwd)/${CERT_MANAGER_MIXIN}/:/data/ \
--S /data/lib/alerts.jsonnet > /data/${CERT_MANAGER_MIXIN}.yaml \
+docker run -v $(pwd)/${CERT_MANAGER_MIXIN}/:/data/ \
+bitnami/jsonnet:latest \
+-S /data/lib/alerts.jsonnet > ${CERT_MANAGER_MIXIN}.yaml
