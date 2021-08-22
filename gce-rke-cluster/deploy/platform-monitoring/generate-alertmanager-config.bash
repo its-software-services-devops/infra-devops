6226eb930ad7dfb21bdebf229ce3bce42c825f54
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-22 22:09:45 +0700

    Added AlertManagerConfig

--- gce-rke-cluster/deploy/platform-monitoring/generate-alertmanager-config.bash
+++ gce-rke-cluster/deploy/platform-monitoring/generate-alertmanager-config.bash
@@ -10,4 +10,5 @@ fi
 export $(cat secrets.txt | sed 's/#.*//g' | xargs)
 
 cp alertmanager-config-tpl.yaml ${OUTPUT_FILE}
-sed -i "s/__SLACK_URL__/${ALERT_NOTI_SLACK_URL}/g" ${OUTPUT_FILE}
+# Use character '#' in the 'sed' command instead of '/' just to escape URL in ALERT_NOTI_SLACK_URL
+sed -i "s#__SLACK_URL__#${ALERT_NOTI_SLACK_URL}#g" ${OUTPUT_FILE}
