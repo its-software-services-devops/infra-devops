4e20feffd4affd73da7dbe9fc39967c10cbc9b85
Seubpong Monsar <pjame.fb@gmail.com>
2021-08-29 13:15:02 +0700

    Patch prometheus

--- gce-rke-cluster/deploy/prometheus/deploy-prometheus-config.bash
+++ gce-rke-cluster/deploy/prometheus/deploy-prometheus-config.bash
@@ -10,3 +10,5 @@ kubectl apply -f prometheus-ing.yaml
 
 kubectl apply -f rendered-grafana-k8s.yaml -n ${NS}
 kubectl apply -f grafana-k8s-ing.yaml -n ${NS}
+
+kubectl patch prometheus k8s -n monitoring --patch --type merge "$(cat prometheus-patch.yaml)"
