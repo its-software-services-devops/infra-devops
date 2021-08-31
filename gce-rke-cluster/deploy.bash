7fe4bd5457cc9805de29a3cbe1f99bdafd1fa47a
seubpong.mon <pjame.fb@gmail.com>
2021-08-31 18:31:21 +0700

    Added minio deployment

--- gce-rke-cluster/deploy.bash
+++ gce-rke-cluster/deploy.bash
@@ -15,7 +15,7 @@ cd deploy/01-basics; ./deploy-basics.bash; cd ${CWD}
 cd deploy/prometheus; ./deploy-prometheus-config.bash; cd ${CWD}
 cd deploy/cluster-logging; ./deploy-cluster-logging.bash; cd ${CWD}
 # Put minio before loki-syslog
-cd deploy/minio-tenants; ./deploy-minio-tenants.bash; cd ${CWD}
+#cd deploy/minio-tenants; ./deploy-minio-tenants.bash; cd ${CWD}
 cd deploy/loki-syslog; ./deploy-loki-syslog.bash; cd ${CWD}
 cd deploy/external-dns; ./deploy-external-dns.bash; cd ${CWD}
 cd deploy/kafka-log-broker; ./deploy-kafka-cluster.bash; cd ${CWD}
@@ -25,4 +25,4 @@ cd deploy/platform-monitoring; ./deploy-platform-monitor.bash; cd ${CWD}
 # Put this to the very end
 cd deploy/certificates; ./deploy-certificates.bash; cd ${CWD}
 # Put this after deploy-certificates.bash
-cd deploy/minio-tenants; ./create-buckets.bash; cd ${CWD}
+#cd deploy/minio-tenants; ./create-buckets.bash; cd ${CWD}
