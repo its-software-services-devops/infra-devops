13cd51953737ded655cdddad748227f1633bc75c
seubpong.mon <pjame.fb@gmail.com>
2021-09-04 19:34:32 +0700

    Added prometheus ing

--- gke-yru-openedx/deploy/prometheus/deploy-prometheus-config.bash
+++ gke-yru-openedx/deploy/prometheus/deploy-prometheus-config.bash
@@ -12,6 +12,9 @@ kubectl apply -f prometheus-ing.yaml -n ${NS}
 kubectl apply -f rendered-grafana-k8s.yaml -n ${NS}
 kubectl apply -f grafana-k8s-ing.yaml -n ${NS}
 
+kubectl apply -f gcp-manage-certs.yaml -n ${NS}
+
+#Set prometheus to monitore in its own namespace
 kubectl label ns monitoring --overwrite monitoring=true
 kubectl patch prometheus k8s -n monitoring --type merge --patch "$(cat prometheus-patch.yaml)"
 
