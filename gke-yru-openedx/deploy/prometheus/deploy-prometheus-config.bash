13581142842a9c2fdbaa1b3de06dbb59f91f3c54
seubpong.mon <pjame.fb@gmail.com>
2021-09-04 18:34:05 +0700

    Added prometheus ing

--- gke-yru-openedx/deploy/prometheus/deploy-prometheus-config.bash
+++ gke-yru-openedx/deploy/prometheus/deploy-prometheus-config.bash
@@ -7,7 +7,7 @@ echo "#### Deploying prometheus config to [${NS}] ####"
 
 kubectl apply -f rendered-prometheus-config.yaml
 #Use this for debug only
-kubectl apply -f prometheus-ing.yaml
+kubectl apply -f prometheus-ing.yaml -n ${NS}
 
 kubectl apply -f rendered-grafana-k8s.yaml -n ${NS}
 kubectl apply -f grafana-k8s-ing.yaml -n ${NS}
