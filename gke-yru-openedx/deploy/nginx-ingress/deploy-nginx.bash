282e0749ece3e6940f7ad1eba966e9619dc53f4c
seubpong.mon <pjame.fb@gmail.com>
2021-09-04 21:52:45 +0700

    Added nginx ingress

--- gke-yru-openedx/deploy/nginx-ingress/deploy-nginx.bash
+++ gke-yru-openedx/deploy/nginx-ingress/deploy-nginx.bash
@@ -5,6 +5,7 @@ NS=ingress-nginx
 echo "####"
 echo "#### Deploying nginx config to [${NS}] ####"
 
-kubectl apply -f rendered-nginx.yaml -n ${NS}
+kubectl apply -f gcp-manage-certs.yaml -n ${NS}
+kubectl apply -f nginx-ing.yaml -n ${NS}
 
-#kubectl annotate svc prometheus-k8s cloud.google.com/neg='{"ingress": true}' --overwrite -n ${NS}
+kubectl apply -f rendered-nginx.yaml -n ${NS}
