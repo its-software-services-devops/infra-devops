ec8b28a657326c9d5acd505e3ce470fa7bce97df
seubpong.mon <pjame.fb@gmail.com>
2021-09-04 22:10:42 +0700

    Added nginx ingress

--- gke-yru-openedx/deploy/prometheus/deploy-prometheus-config.bash
+++ gke-yru-openedx/deploy/prometheus/deploy-prometheus-config.bash
@@ -12,11 +12,6 @@ kubectl apply -f prometheus-ing.yaml -n ${NS}
 kubectl apply -f rendered-grafana-k8s.yaml -n ${NS}
 kubectl apply -f grafana-k8s-ing.yaml -n ${NS}
 
-kubectl apply -f gcp-manage-certs.yaml -n ${NS}
-
 #Set prometheus to monitore in its own namespace
 kubectl label ns monitoring --overwrite monitoring=true
 kubectl patch prometheus k8s -n monitoring --type merge --patch "$(cat prometheus-patch.yaml)"
-
-kubectl annotate svc prometheus-k8s cloud.google.com/neg='{"ingress": true}' --overwrite -n ${NS}
-kubectl annotate svc grafana-k8s cloud.google.com/neg='{"ingress": true}' --overwrite -n ${NS}
