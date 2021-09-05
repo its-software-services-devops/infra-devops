eb7332882af376327452c39ae847606396636ff5
seubpong.mon <pjame.fb@gmail.com>
2021-09-05 07:58:16 +0700

    Set ingress class

--- gke-yru-openedx/deploy/nginx-ingress/deploy-nginx.bash
+++ gke-yru-openedx/deploy/nginx-ingress/deploy-nginx.bash
@@ -8,6 +8,7 @@ echo "#### Deploying nginx config to [${NS}] ####"
 kubectl create ns ${NS}
 
 kubectl apply -f gcp-manage-certs.yaml -n ${NS}
+kubectl apply -f nginx-backend-cfg.yaml -n ${NS}
 kubectl apply -f nginx-ing.yaml -n ${NS}
 
 kubectl apply -f rendered-nginx.yaml -n ${NS}
